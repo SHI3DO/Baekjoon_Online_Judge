bcc628fe70a5ad732d81c7c021db986784f5ab55
SHI3DO <61821471+SHI3DO@users.noreply.github.com>
2022-05-24 11:58:05 +0900

    Create 11866.py

---
+++ 11866/11866.py
@@ -0,0 +1,16 @@
+import sys
+
+a, b = map(int, sys.stdin.readline().split())
+
+que = []
+pr = []
+for i in range(1, a + 1):
+    que.append(i)
+
+num = 0
+while len(que) > 0:
+    num += b-1
+    if num >= len(que):
+        num = num % len(que)
+    pr.append(que.pop(num))
+print(f"<{str(pr)[1:-1]}>")
